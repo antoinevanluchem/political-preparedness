75bd2c25452c5a0384080d1864e882fcbc316231
Antoine Van Luchem <antoine.van-luchem@bmw.de>
2024-08-09 17:01:30 +0200

    Implement RepresentativeListAdapter

--- starter/app/src/main/java/com/example/android/politicalpreparedness/representative/adapter/RepresentativeListAdapter.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/representative/adapter/RepresentativeListAdapter.kt
@@ -3,77 +3,75 @@ package com.example.android.politicalpreparedness.representative.adapter
 import android.content.Intent
 import android.content.Intent.ACTION_VIEW
 import android.net.Uri
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.example.android.politicalpreparedness.R
-import com.example.android.politicalpreparedness.databinding.ViewholderRepresentativeBinding
+import com.example.android.politicalpreparedness.databinding.RepresentativeListItemBinding
 import com.example.android.politicalpreparedness.network.models.Channel
-import com.example.android.politicalpreparedness.network.models.Election
 import com.example.android.politicalpreparedness.representative.model.Representative
 
-class RepresentativeListAdapter: ListAdapter<Representative, ViewHolder>(RepresentativeDiffCallback()){
+class RepresentativeListAdapter :
+    ListAdapter<Representative, RepresentativeListAdapter.ViewHolder>(DiffCallback) {
+    //
+    // ViewHolder
+    //
+    class ViewHolder(val binding: RepresentativeListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder.from(parent)
-    }
+        fun bind(item: Representative) {
+            binding.representative = item
+            // TODO-Antoine: set the representative phote
+//        binding.representativePhoto.setImageResource(R.drawable.ic_profile)
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = getItem(position)
-        holder.bind(item)
-    }
-}
+            //TODO: Show social links ** Hint: Use provided helper methods
+            //TODO: Show www link ** Hint: Use provided helper methods
 
-class ViewHolder(val binding: ViewholderRepresentativeBinding): RecyclerView.ViewHolder(binding.root) {
+            binding.executePendingBindings()
+        }
 
-    fun bind(item: Representative) {
-        binding.representative = item
-        binding.representativePhoto.setImageResource(R.drawable.ic_profile)
+        //TODO: Add companion object to inflate ViewHolder (from)
 
-        //TODO: Show social links ** Hint: Use provided helper methods
-        //TODO: Show www link ** Hint: Use provided helper methods
+        private fun showSocialLinks(channels: List<Channel>) {
+            val facebookUrl = getFacebookUrl(channels)
+            if (!facebookUrl.isNullOrBlank()) {
+                enableLink(binding.facebookIcon, facebookUrl)
+            }
 
-        binding.executePendingBindings()
-    }
+            val twitterUrl = getTwitterUrl(channels)
+            if (!twitterUrl.isNullOrBlank()) {
+                enableLink(binding.twitterIcon, twitterUrl)
+            }
+        }
 
-    //TODO: Add companion object to inflate ViewHolder (from)
+        private fun showWWWLinks(urls: List<String>) {
+            // TODO-Antoine: fix this
+//        enableLink(binding.wwwIcon, urls.first())
+        }
 
-    private fun showSocialLinks(channels: List<Channel>) {
-        val facebookUrl = getFacebookUrl(channels)
-        if (!facebookUrl.isNullOrBlank()) { enableLink(binding.facebookIcon, facebookUrl) }
+        private fun getFacebookUrl(channels: List<Channel>): String? {
+            return channels.filter { channel -> channel.type == "Facebook" }
+                .map { channel -> "https://www.facebook.com/${channel.id}" }.firstOrNull()
+        }
 
-        val twitterUrl = getTwitterUrl(channels)
-        if (!twitterUrl.isNullOrBlank()) { enableLink(binding.twitterIcon, twitterUrl) }
-    }
+        private fun getTwitterUrl(channels: List<Channel>): String? {
+            return channels.filter { channel -> channel.type == "Twitter" }
+                .map { channel -> "https://www.twitter.com/${channel.id}" }.firstOrNull()
+        }
 
-    private fun showWWWLinks(urls: List<String>) {
-        enableLink(binding.wwwIcon, urls.first())
-    }
+        private fun enableLink(view: ImageView, url: String) {
+            view.visibility = View.VISIBLE
+            view.setOnClickListener { setIntent(url) }
+        }
 
-    private fun getFacebookUrl(channels: List<Channel>): String? {
-        return channels.filter { channel -> channel.type == "Facebook" }
-                .map { channel -> "https://www.facebook.com/${channel.id}" }
-                .firstOrNull()
-    }
-
-    private fun getTwitterUrl(channels: List<Channel>): String? {
-        return channels.filter { channel -> channel.type == "Twitter" }
-                .map { channel -> "https://www.twitter.com/${channel.id}" }
-                .firstOrNull()
-    }
-
-    private fun enableLink(view: ImageView, url: String) {
-        view.visibility = View.VISIBLE
-        view.setOnClickListener { setIntent(url) }
-    }
-
-    private fun setIntent(url: String) {
-        val uri = Uri.parse(url)
-        val intent = Intent(ACTION_VIEW, uri)
-        itemView.context.startActivity(intent)
+        private fun setIntent(url: String) {
+            val uri = Uri.parse(url)
+            val intent = Intent(ACTION_VIEW, uri)
+            itemView.context.startActivity(intent)
+        }
     }
 
     //
@@ -90,6 +88,21 @@ class ViewHolder(val binding: ViewholderRepresentativeBinding): RecyclerView.Vie
         }
     }
 
+    //
+    // onX-functions
+    //
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            RepresentativeListItemBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item)
+    }
 }
 
 //TODO: Create RepresentativeListener
