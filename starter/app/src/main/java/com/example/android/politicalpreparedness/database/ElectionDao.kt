fd23161d680b1c36104b405d0b41908619d8d416
Antoine Van Luchem <antoine.van-luchem@bmw.de>
2024-08-12 09:59:20 +0200

    Implement CivicsApiService and ElectionDao

--- starter/app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
@@ -1,7 +1,9 @@
 package com.example.android.politicalpreparedness.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import com.example.android.politicalpreparedness.network.models.Election
@@ -9,14 +11,18 @@ import com.example.android.politicalpreparedness.network.models.Election
 @Dao
 interface ElectionDao {
 
-    //TODO: Add insert query
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(election: Election): Long
 
-    //TODO: Add select all election query
+    @Query("SELECT * FROM election_table WHERE ID=:id")
+    fun getElection(id:Int): Election?
 
-    //TODO: Add select single election query
+    @Query("SELECT * FROM election_table")
+    fun getAllElections(): LiveData<List<Election>?>
 
-    //TODO: Add delete query
+    @Query("DELETE FROM election_table WHERE ID=:id")
+    suspend fun deleteElection(id: Int)
 
-    //TODO: Add clear query
-
+    @Query("DELETE FROM election_table")
+    suspend fun clearElections()
 }
