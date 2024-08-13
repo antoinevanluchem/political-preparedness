90f8c04fbdcdd8b20da40d5041ebb77b37c19372
Antoine Van Luchem <antoine.van-luchem@bmw.de>
2024-08-13 11:45:16 +0200

    Beautify

--- starter/app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
@@ -15,14 +15,11 @@ interface ElectionDao {
     suspend fun insert(election: Election): Long
 
     @Query("SELECT * FROM election_table WHERE ID=:id")
-    fun getElection(id:Int): Election?
+    fun getElection(id: Int): Election?
 
     @Query("SELECT * FROM election_table")
     fun getAllElections(): LiveData<List<Election>?>
-//
+
     @Query("DELETE FROM election_table WHERE ID=:id")
     suspend fun deleteElection(id: Int)
-//
-//    @Query("DELETE FROM election_table")
-//    suspend fun clearElections()
 }
